b01d85c60a448099c294adc682dd57008f1d43bb
Nadia <115858781+Nadia8666@users.noreply.github.com>
2024-10-23 11:19:34 -0700

    Update MixinWorldServer.java

--- src/main/java/net/smileycorp/serverpauser/mixin/MixinWorldServer.java
+++ src/main/java/net/smileycorp/serverpauser/mixin/MixinWorldServer.java
@@ -1,6 +1,9 @@
 package net.smileycorp.serverpauser.mixin;
 
+import net.minecraft.server.MinecraftServer;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -15,14 +18,18 @@ import net.minecraft.world.storage.WorldInfo;
 @Mixin(WorldServer.class)
 public abstract class MixinWorldServer extends World {
 
+	@Shadow @Final private MinecraftServer mcServer;
+
 	protected MixinWorldServer(ISaveHandler saveHandlerIn, WorldInfo info, WorldProvider providerIn,
-			Profiler profilerIn, boolean client) {
+							   Profiler profilerIn, boolean client) {
 		super(saveHandlerIn, info, providerIn, profilerIn, client);
 	}
 
 	@Inject(at=@At("HEAD"), method = "tick()V", cancellable = true)
 	public void tick(CallbackInfo callback) {
-		if (playerEntities.isEmpty()) callback.cancel();
+		if (mcServer.getPlayerList().getPlayers().isEmpty()) {
+			callback.cancel();
+		};
 	}
 
 }
